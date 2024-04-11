2e3d968280d950f1384b5b4f076da4e0f69fb487
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2024-04-11 23:06:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Idea: Webpage that shows you lines from a movie and you get a certain number of guesses to guess the movie and a new line with each guess
+# a data source for lines from a movie or atleast transcripts
+# split the transcript or just sort the lines by the time they appear or frequency of phrase
+# a user interface where we can show the user lines and recieve input on the web
+# axios python to get the lines from the movie and check the user's input
+# use this api: https://pypi.org/project/tmdbv3api/#:~:text=The%20TMDb%20API%20is%20a,free%20and%20community%20edited%20database.
+# https://www.themoviedb.org/
+
+
